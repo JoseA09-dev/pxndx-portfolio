ebe018fb5dd7648e2415062fbbf8f21622ee0c3d
JoseA09-dev <201001853+JoseA09-dev@users.noreply.github.com>
2025-03-03 12:13:56 -0500

    example-home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,8 @@
-
+function Home() {
+    return (
+      <section className="home">
+        <h1>Bienvenido a mi Portafolio</h1>
+        <p>Explora mis proyectos y conoce más sobre mi trabajo.</p>
+      </section>
+    );
+  }
