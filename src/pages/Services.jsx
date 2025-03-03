97c33ebb3126ec292e59aadcf0796664d88a8766
JoseA09-dev <201001853+JoseA09-dev@users.noreply.github.com>
2025-03-03 12:20:39 -0500

    example-services.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,1 +1,11 @@
-
+function Services() {
+    return (
+      <section className="services">
+        <h1>Servicios</h1>
+        <p>Ofrezco desarrollo web, diseño UI/UX y soluciones digitales personalizadas.</p>
+      </section>
+    );
+  }
+  
+export default Services;
+  
