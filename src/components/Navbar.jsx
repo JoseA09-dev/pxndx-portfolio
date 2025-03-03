f42467c0853cecefb3f0fce5e6145c4edceef5c0
JoseA09-dev <201001853+JoseA09-dev@users.noreply.github.com>
2025-03-03 11:00:32 -0500

    example-navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,18 @@
+import { Link } from "react-router-dom";
+import "../styles/globals.css";
 
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><Link to="/">Inicio</Link></li>
+        <li><Link to="/about">Sobre mí</Link></li>
+        <li><Link to="/projects">Proyectos</Link></li>
+        <li><Link to="/services">Servicios</Link></li>
+        <li><Link to="/contact">Contacto</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
