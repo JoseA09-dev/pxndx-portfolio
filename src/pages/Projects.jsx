0e004db86e5bde5cee4f0813bba2d90e2a7510e5
JoseA09-dev <201001853+JoseA09-dev@users.noreply.github.com>
2025-03-03 12:20:10 -0500

    example-projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,1 +1,11 @@
-
+function Projects() {
+    return (
+      <section className="projects">
+        <h1>Mis Proyectos</h1>
+        <p>Aquí puedes ver algunos de los proyectos en los que he trabajado.</p>
+      </section>
+    );
+  }
+  
+export default Projects;
+  
