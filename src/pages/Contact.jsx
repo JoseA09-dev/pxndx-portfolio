b92d2cb5ea0fdbb30413b47811600ca647068552
JoseA09-dev <201001853+JoseA09-dev@users.noreply.github.com>
2025-03-03 12:22:07 -0500

    example-contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,1 +1,10 @@
-
+function Contact() {
+    return (
+      <section className="contact">
+        <h1>Contacto</h1>
+        <p>Puedes comunicarte conmigo a través del siguiente formulario o por correo electrónico.</p>
+      </section>
+    );
+  }
+  
+  export default Contact;
