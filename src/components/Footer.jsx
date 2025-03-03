4c79a91dc249e813182ccb22bfd750a3f89e5a40
JoseA09-dev <201001853+JoseA09-dev@users.noreply.github.com>
2025-03-03 11:01:04 -0500

    example-footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,9 @@
-
+function Footer() {
+    return (
+      <footer className="footer">
+        <p>© 2024 Mi Portafolio. Todos los derechos reservados.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
