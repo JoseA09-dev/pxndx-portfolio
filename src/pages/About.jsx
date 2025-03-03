0b56a08a55bf100b62bfe30f78412f27fba78e4a
JoseA09-dev <201001853+JoseA09-dev@users.noreply.github.com>
2025-03-03 12:15:39 -0500

    example-about.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,1 +1,10 @@
-
+function About() {
+    return (
+      <section className="about">
+        <h1>Sobre Mí</h1>
+        <p>Soy un diseñador web y desarrollador frontend apasionado por crear experiencias digitales increíbles.</p>
+      </section>
+    );
+  }
+  
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,3 +6,5 @@ function Home() {
       </section>
     );
   }
+
+export default Home;
